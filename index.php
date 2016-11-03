44f0ea11973585073b8b20a3ffc66862688e8144
Simon <simonbesseling@live.nl>
2016-11-03 10:14:42 +0100

    Improved the dashboard table

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
-<?php
-require_once('assets/head.php');
-require_once('connection.php');
-?>
+  <?php
+    require_once('assets/head.php');
+    require_once('connection.php');
+  ?>
 <body>
     <?php require_once('assets/header.php'); ?>
 
@@ -14,15 +14,16 @@ require_once('connection.php');
 
     <?php
     //Prepare and run a query
-    $getCalendar = $dbh->prepare("SELECT `calendar`.*, `user1`.`username` AS `docent1`, `user2`.`username` AS `docent2`, `student`.`first_name` as `student_first_name` FROM `calendar`
-                                 LEFT JOIN `user` as `user1`
-                                 ON user1.id = calendar.user_id_1
-                                 LEFT JOIN `user` as `user2`
-                                 ON user2.id = calendar.user_id_2
-                                 LEFT JOIN `student`
-                                 ON student.id = calendar.student_id
-                                 WHERE DATE(date)=CURDATE()
-                                 ORDER BY date ASC, time ASC");
+    $getCalendar = $dbh->prepare("SELECT `calendar`.*, `user1`.`username` AS `docent1`, `user2`.`username` AS `docent2`, `student`.`first_name` AS `student_first_name`, `student`.`last_name` AS `student_last_name`
+                                  FROM `calendar`
+                                  LEFT JOIN `user` AS `user1`
+                                  ON user1.id = calendar.user_id_1
+                                  LEFT JOIN `user` AS `user2`
+                                  ON user2.id = calendar.user_id_2
+                                  LEFT JOIN `student`
+                                  ON student.id = calendar.student_id
+                                  WHERE DATE(date)=CURDATE()
+                                  ORDER BY date ASC, time ASC");
     $getCalendar->execute();
     $calendar = $getCalendar->fetchAll();
 
@@ -31,26 +32,20 @@ require_once('connection.php');
     echo "<table>";
     echo "<thead>";
     echo "<tr>
-            <th>ID</th>
-            <th>Docent 1</th>
-            <th>Docent 2</th>
+            <th>Naam</th>
+            <th>Examinatoren</th>
             <th>Datum</th>
             <th>Tijd</th>
-            <th>Examen</th>
-            <th>Leerling</th>
           </tr>";
     echo "</thead>";
 
     echo "<tbody>";
     foreach( $calendar as $row) {
       echo "<tr>";
-      echo "<td>".$row['id']."</td>";
-      echo "<td>".$row['docent1']."</td>";
-      echo "<td>".$row['docent2']."</td>";
+      echo "<td>".$row['student_first_name']."&nbsp;".$row['student_last_name']."</td>";
+      echo "<td>".$row['docent1']."<span> & </span>".$row['docent2']."</td>";
       echo "<td>".$row['date']."</td>";
       echo "<td>".$row['time']."</td>";
-      echo "<td>".$row['exam_id']."</td>";
-      echo "<td>".$row['student_first_name']."</td>";
       echo "</tr>";
     }
     echo "</tbody>";
