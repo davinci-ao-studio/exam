ae59ddb986b6461fb49c041ce22bac629ec45c2e
Simon <simonbesseling@live.nl>
2016-11-15 14:29:20 +0100

    completed the index.php page with bootstrap and mvc

--- application/models/Exam_model.php
+++ application/models/Exam_model.php
@@ -10,8 +10,13 @@ class Exam_model extends CI_Model {
 
         public function getData()
         {
-          $result = $this->db->get('calendar')->result_array();
-          //$result->db->join('student', 'student_id = calendar.student.id', 'left');
+          $this->db->select("`calendar`.*, `user1`.`username` AS `docent1`, `user2`.`username` AS `docent2`, `student`.`first_name` AS `student_first_name`, `student`.`last_name` AS `student_last_name`");
+          $this->db->from("calendar");
+          $this->db->join('user` AS `user1', 'user1.id = calendar.user_id_1', 'left');
+          $this->db->join('user` AS `user2', 'user2.id = calendar.user_id_2', 'left');
+          $this->db->join('student', 'student.id = calendar.student_id', 'left');
+          $result = $this->db->get()->result_array();
+
           return $result;
         }
 }
--- application/views/Exam/index.php
+++ application/views/Exam/index.php
@@ -1,57 +1,46 @@
 <!DOCTYPE html>
 <html>
-<body>
-    <?php //require_once('assets/header.php'); ?>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/master.css">
+    <meta charset="utf-8">
+    <title>Examens ICT Da Vinci College</title>
+    <link rel="stylesheet" href="/examen_automatiseren/css/master.css" type="text/css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  </head>
+  <body>
     <div class="container">
       <div class="row col-md-6">
         <main>
           <h1>Home</h1>
           <article class="todays_exams">
-          <h2>Opkomende Examens</h2>
-
+            <h2>Opkomende Examens</h2>
             <?php
-            //Prepare and run a query
-            /*$getCalendar = $dbh->prepare("SELECT `calendar`.*, `user1`.`username` AS `docent1`, `user2`.`username` AS `docent2`, `student`.`first_name` AS `student_first_name`, `student`.`last_name` AS `student_last_name`
-                                          FROM `calendar`
-                                          LEFT JOIN `user` AS `user1`
-                                          ON user1.id = calendar.user_id_1
-                                          LEFT JOIN `user` AS `user2`
-                                          ON user2.id = calendar.user_id_2
-                                          LEFT JOIN `student`
-                                          ON student.id = calendar.student_id
-                                          WHERE DATE(date)=CURDATE()
-                                          ORDER BY date ASC, time ASC");
-            $getCalendar->execute();
-            $calendar = $getCalendar->fetchAll();*/
-
-            //Create the table
-            echo "<div class='table-responsive'>";
-            echo "<table class='table'>";
-            echo "<thead>";
-            echo "<tr>
-                    <th>Naam</th>
-                    <th>Examinatoren</th>
-                    <th>Datum</th>
-                    <th>Tijd</th>
-                  </tr>";
-            echo "</thead>";
-
-            echo "<tbody>";
-            foreach( $result as $row) {
-              echo "<tr>";
-              echo "<td>".$row['student_id']."</td>";
-              echo "<td>".$row['user_id_1']."</td>";
-              echo "<td>".$row['date']."</td>";
-              echo "<td>".$row['time']."</td>";
-              echo "</tr>";
-            }
-            echo "</tbody>";
-            echo "</table>";
-            echo "</div>";
-          ?>
+              echo "<div class='table-responsive'>";
+              echo "<table class='table'>";
+              echo "<thead>";
+              echo "<tr>
+                <th>Naam</th>
+                <th>Examinatoren</th>
+                <th>Datum</th>
+                <th>Tijd</th>
+              </tr>";
+              echo "</thead>";
+              echo "<tbody>";
+              foreach( $result as $row) {
+                echo "<tr>";
+                  echo "<td>".$row['student_first_name']."&nbsp;".$row['student_last_name']."</td>";
+                  echo "<td>".$row['docent1']."<span> & </span>".$row['docent2']."</td>";
+                  echo "<td>".$row['date']."</td>";
+                  echo "<td>".$row['time']."</td>";
+                echo "</tr>";
+              }
+              echo "</tbody>";
+              echo "</table>";
+              echo "</div>";
+            ?>
           </article>
         </main>
-       </div>
       </div>
+    </div>
   </body>
 </html>
